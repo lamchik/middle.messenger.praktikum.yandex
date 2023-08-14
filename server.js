ee1496a3a506a81906e81a79907090588d0abfdc
lamara <lamaralanchava@gmail.com>
2023-08-14 19:59:48 +0300

    node version

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-import { fileURLToPath } from 'url';
+// const {fileURLToPath} = require('url');
 
 const app = express();
 const PORT = 3000;
 
-const current = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(current);
+// const current = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname("/");
 
-app.use(express.static(__dirname +'/dist'))
+app.use(express.static(path.join(__dirname, "/dist")))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
