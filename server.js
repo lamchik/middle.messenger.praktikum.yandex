a0afbea6a8bd59bc2e57e90150f2b4706e7b9919
lamara <lamaralanchava@gmail.com>
2023-08-15 00:12:45 +0300

    bug fixed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
-// const {fileURLToPath} = require('url');
+import express from 'express';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
 
-// const current = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname("/");
+const current = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(current);
 
-app.use(express.static(path.join(__dirname, "/dist")))
+app.use(express.static(__dirname +'/dist'))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
