4542e12f97d1a0d72dd3bb483fe48584948029d4
lamara <lamaralanchava@gmail.com>
2023-08-14 19:29:24 +0300

    hode version

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const current = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(current);
 
 app.use(express.static(__dirname +'/dist'))
-app.set("view engine", "hbs");
-app.set("views", path.resolve(__dirname + '/dist'));
-app.get("*", (req, res) => {
-  res.status(200).sendFile("/dist/index.html", {
-    root: __dirname + "/"
-  });
-});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
